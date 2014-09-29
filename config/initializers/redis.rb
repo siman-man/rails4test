eda306e10b8a8b2d8c862a9bfe500e8404ff0c56
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-09-30 00:08:53 +0900

    update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,7 @@
 require 'redis'
-REDIS = Redis.new(host: '127.0.0.1', port: 6379, driver: :hiredis)
+if ENV["REDISTOGO_URL"] != nil
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, driver: :hiredis)
+else
+    REDIS = Redis.new(host: '127.0.0.1', port: 6379, driver: :hiredis)
+end
